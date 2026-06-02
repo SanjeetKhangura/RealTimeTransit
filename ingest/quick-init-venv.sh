340dd507092d17cf8866e585bb0c25c2cabdeaba
sniff <sniff@sniff.cc>
2026-06-01 20:07:53 -0700

    can pull from gtfs!

--- ingest/quick-init-venv.sh
+++ ingest/quick-init-venv.sh
@@ -1,4 +1,5 @@
 #/usr/bin/env bash
+# run as `source` to activate venv in current shell!
 # lazy dev's script to venv and install deps 
 # todo: replace this garbage with uv
 python3 -m venv .venv
--- ingest/realtimetransit_ingest.py
+++ ingest/realtimetransit_ingest.py
@@ -1,7 +1,10 @@
+#!/usr/bin/env python3
+
 import logging
 import os
 import argparse
 import requests
+import psycopg
 from datetime import datetime
 from google.transit import gtfs_realtime_pb2
 
@@ -24,26 +27,26 @@ if __name__ == "__main__":
         help='base url of the gtfs feed (including trailing slash)'
     )
     parser.add_argument(
-        '--gtfs_endpoint_trip_updates', '-u',
+        '--gtfs_endpoint_trip_updates', '-T',
         type=str,
-        default=os.getenv('GTFS_ENDPOINT_TRIP_UPDATES', 'gtfsposition'),
+        default=os.getenv('GTFS_ENDPOINT_TRIP_UPDATES', 'gtfsrealtime'),
         help='endpoint for trip updates feed (appended to base url)'
     )
     parser.add_argument(
-        '--gtfs_endpoint_vehicle_positions', '-v',
+        '--gtfs_endpoint_vehicle_positions', '-V',
         type=str,
         default=os.getenv('GTFS_ENDPOINT_VEHICLE_POSITIONS', 'gtfsposition'),
         help='endpoint for vehicle positions feed (appended to base url)'
     )
     parser.add_argument(
-        '--gtfs_endpoint_service_alerts', '-s',
+        '--gtfs_endpoint_service_alerts', '-A',
         type=str,
         default=os.getenv('GTFS_ENDPOINT_SERVICE_ALERTS', 'gtfsalerts'),
         help='endpoint for service alerts feed (appended to base url)'
     )
 
     parser.add_argument(
-        '--validation_options', '-V',
+        '--validation_options', '-O',
         type=str,
         default=os.getenv('VALIDATION_OPTIONS'),
         help='json of validator config optoins'
@@ -107,6 +110,11 @@ if __name__ == "__main__":
         help='logging level (DEBUG, INFO, WARNING, ERROR, CRITICAL)'
     )
     parser.add_argument(
+        '--verbose', '-v',
+        action='store_true',
+        help='enable verbose logging (log level DEBUG)'
+    )
+    parser.add_argument(
         '--log_file', '-f',
         type=str,
         default=os.getenv('LOG_FILE', 'ingest{}.log'.format(datetime.now().strftime('%Y%m%d_%H%M%S'))),
@@ -122,7 +130,7 @@ if __name__ == "__main__":
 
     # set up logging
     logging.basicConfig(
-        level=args.log_level,
+        level=(logging.DEBUG if args.verbose else getattr(logging, args.log_level.upper(), logging.INFO)),
         handlers=[
             logging.FileHandler(args.log_file),
             logging.StreamHandler()
@@ -130,7 +138,7 @@ if __name__ == "__main__":
     )
     logging.debug("Parsed arguments: {}".format(args))
 
-    # set up timescaledb connection
+    # figure out up postgres connection string
     if args.db:
         db_connection_string = args.db
     else:
@@ -143,27 +151,43 @@ if __name__ == "__main__":
         )
     logging.debug("Database connection string: {}".format(db_connection_string))
 
-    # fetch feed
-    logging.info("Fetching feed from {}".format(args.url))
+    # set up feed urls if set
+    feed_urls = {}
+    if args.gtfs_endpoint_trip_updates:
+        feed_urls['trip_updates'] = args.gtfs_base_url + args.gtfs_endpoint_trip_updates
+    if args.gtfs_endpoint_vehicle_positions:
+        feed_urls['vehicle_positions'] = args.gtfs_base_url + args.gtfs_endpoint_vehicle_positions
+    if args.gtfs_endpoint_service_alerts:
+        feed_urls['service_alerts'] = args.gtfs_base_url + args.gtfs_endpoint_service_alerts
+    logging.debug("Feed URLs: {}".format(feed_urls))
+    query_params = {'apikey': args.api_key} if args.api_key else {} # python syntax is stupid (why does "if" come after???)
+    
     feed = gtfs_realtime_pb2.FeedMessage()
-    try:
-        response = requests.get(
-            args.url,
-            headers={'Authorization': 'Bearer {}'.format(args.api_key)},
-            timeout=args.timeout
-        )
-        response.raise_for_status()
-        feed.ParseFromString(response.content)
-        logging.info("Successfully fetched feed with {} entities".format(len(feed.entity)))
-    except requests.exceptions.RequestException as e:
-        logging.error("Error fetching feed: {}".format(e))
-        exit(1)
-    except Exception as e:
-        logging.error("Error parsing feed: {}".format(e))
-        exit(1)
+    for feed_name, feed_url in feed_urls.items():
+        logging.info("Fetching {} from {}".format(feed_name, feed_url))
+        try:
+            response = requests.get(
+                feed_url,
+                params=query_params,
+                timeout=args.timeout
+            )
+            response.raise_for_status()
+            lazybadvar = len(feed.entity) #bad bad
+            feed.ParseFromString(response.content)
+            logging.info("Successfully fetched {} feed with {} entities".format(feed_name, len(feed.entity)-lazybadvar))
+        except requests.exceptions.RequestException as e:
+            logging.error("Error fetching {} feed: {}".format(feed_name, e))
+        except Exception as e:
+            logging.error("Error parsing {} feed: {}".format(feed_name, e))
 
     # todo: validate data for invalid or missing fields (and compare to static schedule)
     
-    #//
+    if (args.validation_options):
+        logging.warn("not implemented ({})".format(args.validation_options))
+    
+    # temp dump to console
+    logging.debug("Feed content: {}".format(repr(feed)))
 
-    # todo: upsert data into timescaledb
+    # todo: copy data into timescaledb
+
+    #with psycopg.connect(db_connection_string) as conn:
