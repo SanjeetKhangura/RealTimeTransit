19fb226ee1b73b9c621a2b7783200f8cbf702694
Kumardeep Singh <Kumardeepsingh@student.kpu.ca>
2026-07-14 14:55:25 -0700

    feat(models): add ServiceAlertEntity model

--- api/models/service_alert.go
+++ api/models/service_alert.go
@@ -12,3 +12,15 @@ type ServiceAlert struct {
 	StartTime       *time.Time `db:"start_time"       json:"startTime"`
 	EndTime         *time.Time `db:"end_time"         json:"endTime"`
 }
+
+// ServiceAlertEntity represents one informed entity for a service alert.
+// One alert can have multiple entities linking it to routes, trips, and stops.
+type ServiceAlertEntity struct {
+	Ts          time.Time `db:"ts"`
+	AlertID     string    `db:"alert_id"`
+	AgencyID    *string   `db:"agency_id"`
+	RouteID     *string   `db:"route_id"`
+	TripID      *string   `db:"trip_id"`
+	StopID      *string   `db:"stop_id"`
+	DirectionID *int      `db:"direction_id"`
+}
