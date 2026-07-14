18bfd9105a0f8bb6c287591db160dae3ee827b48
Kumardeep Singh <Kumardeepsingh@student.kpu.ca>
2026-07-14 15:04:35 -0700

    fix(models): clean up struct tag whitespace in TripUpdate

--- api/models/trip_update.go
+++ api/models/trip_update.go
@@ -3,14 +3,14 @@ package models
 import "time"
 
 type TripUpdate struct {
-	TS     					time.Time 		`db:"ts" 					json:"ts"`
-	TripID        			string    		`db:"trip_id" 				json:"tripId"`
-	RouteID       			string    		`db:"route_id" 				json:"routeId"`
-	StopID		 			string    		`db:"stop_id" 				json:"stopId"`
-	StopSequence    		*int       		`db:"stop_sequence" 		json:"stopSequence"`
-	ArrivalDelay   			*int       		`db:"arrival_delay" 		json:"arrivalDelay"`
-	ArrivalTime    			*time.Time 		`db:"arrival_time" 			json:"arrivalTime"`
-	DeoartureDelay  		*int       		`db:"departure_delay" 		json:"departureDelay"`
-	DepartureTime  			*time.Time 		`db:"departure_time" 		json:"departureTime"`
-	ScheduleRelationship 	*string    		`db:"schedule_relationship" json:"scheduleRelationship"`
+	TS                   time.Time  `db:"ts"                    json:"ts"`
+	TripID               string     `db:"trip_id"               json:"tripId"`
+	RouteID              string     `db:"route_id"              json:"routeId"`
+	StopID               string     `db:"stop_id"               json:"stopId"`
+	StopSequence         *int       `db:"stop_sequence"         json:"stopSequence"`
+	ArrivalDelay         *int       `db:"arrival_delay"         json:"arrivalDelay"`
+	ArrivalTime          *time.Time `db:"arrival_time"          json:"arrivalTime"`
+	DeoartureDelay       *int       `db:"departure_delay"       json:"departureDelay"`
+	DepartureTime        *time.Time `db:"departure_time"        json:"departureTime"`
+	ScheduleRelationship *string    `db:"schedule_relationship" json:"scheduleRelationship"`
 }
